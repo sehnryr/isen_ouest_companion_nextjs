177958ad99b0e4c1dec226247bb7cd4b420f38e5
Youn Mélois <youn@melois.dev>
2022-09-09 20:07:13 +0200

    fix deployment issue (#8)

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: isProd ? '/isen_ouest_companion/' : undefined,
   images: {
     loader: 'akamai',
-    path: '/isen_ouest_companion',
+    path: '',
   },
 }
 
