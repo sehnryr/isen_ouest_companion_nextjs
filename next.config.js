9fa01f02376296255406df5fe4e984ea32a2c0da
sehnryr <youn@melois.dev>
2022-09-09 20:06:48 +0200

    add conditional asset prefix when on prod

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: isProd ? '/isen_ouest_companion/' : undefined,
   images: {
     loader: 'akamai',
     path: '',
