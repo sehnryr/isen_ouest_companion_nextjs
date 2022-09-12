56be63fee32de3238b1429a01f12996db1b3b66d
sehnryr <youn@melois.dev>
2022-09-12 15:12:59 +0000

    Deploying to gh-pages from @ sehnryr/isen_ouest_companion@77f5b16cdde8e85e681a045e3f8e10a7284149f6 🚀

--- _next/static/OwaGaA8n-gMLC6J04ggSM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/539-c54fbe922dbdfc89.js","static/chunks/pages/index-52f1525626a3589b.js"],"/_error":["static/chunks/pages/_error-fb68742d3cf986b6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
